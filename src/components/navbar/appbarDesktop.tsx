033fe7173b8169a0f1ff21f489a2473a43b0a650
banexxl <damjanovic.branislav@gmail.com>
2023-01-23 11:10:01 +0100

    popravka bilda

--- src/components/navbar/appbarDesktop.tsx
+++ src/components/navbar/appbarDesktop.tsx
@@ -3,7 +3,7 @@ import { AppbarContainer, AppbarHeader, MyList } from "../../styles/appbar"
 import SearchIcon from "@mui/icons-material/Search";
 import Actions from "./actions";
 import { useUIContext } from "../../context/ui";
-import CartDialog from '../cart/index'
+import CartDialog from '../../pages/cart'
 
 export default function AppbarDesktop({ isScreenToMedium }: any) {
 
