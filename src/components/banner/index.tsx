68b483447fcaae55fa0a7a0aaad05a6102d71965
banexxl <damjanovic.branislav@gmail.com>
2023-04-02 18:08:11 +0200

    da vidim na sta lici banner na fonu

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -3,6 +3,7 @@ import { useTheme } from "@mui/system"
 import Link from "next/link";
 import Image from "next/image";
 import { BannerContainer, BannerContent, BannerImage, BannerShopButton, BannerText, BannerTitle } from "../../styles/banner"
+import { Colors } from "@/styles/theme";
 
 
 export default function Banner() {
@@ -19,9 +20,10 @@ export default function Banner() {
                                                   <BannerTitle sx={{ textAlign: 'right' }}>
                                                             Apoteka DAR
                                                   </BannerTitle>
-                                                  Dobro došli u našu apoteku, gde se pružaju visokokvalitetni proizvodi i usluge.
-                                                  Mi smo tu da Vam pomognemo da pronađete ono što Vam je potrebno, bilo da se radi o lekovima ili osvežavajućim proizvodima za negu.
-                                                  Imamo stručan tim koji će Vam pružiti savet i pomoć u bilo kojem trenutku. Posetite nas i uverite se u našu kvalitet i uslugu. Hvala što ste nas odabrali!
+                                                  Dobro došli u <Typography sx={{ color: 'darkred', textAlign: 'justify', display: 'inline', fontSize: '.9rem', fontWeight: 'bold' }}>našu i Vašu apoteku</Typography>, gde se pružaju
+                                                  <Typography sx={{ color: 'darkred', textAlign: 'justify', display: 'inline', fontSize: '.9rem', fontWeight: 'bold' }}> visokokvalitetni proizvodi i usluge.</Typography>
+                                                  <Typography sx={{ color: 'darkred', textAlign: 'justify', display: 'inline', fontSize: '.9rem', fontWeight: 'bold' }}>Mi smo tu da Vam pomognemo</Typography> da pronađete ono što Vam je potrebno, bilo da se radi o lekovima ili osvežavajućim proizvodima za negu.
+                                                  Imamo stručan tim koji će Vam pružiti <Typography sx={{ color: 'darkred', textAlign: 'justify', display: 'inline', fontSize: '.9rem', fontWeight: 'bold' }}>savet i pomoć </Typography>u bilo kojem trenutku. Posetite nas i uverite se u našu kvalitet i uslugu. Hvala što ste nas odabrali!
                                         </BannerText>
                                         <BannerShopButton color="primary" >
                                                   <Link href="/products">Proizvodi</Link>
