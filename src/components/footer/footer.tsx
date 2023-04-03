2642aedab78c7d7c42cf4925f907c2ab634d47d2
banexxl <damjanovic.branislav@gmail.com>
2023-04-03 22:48:45 +0200

    dja mi se futer :)

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,37 +1,49 @@
 import styled from "@emotion/styled";
-import { List, ListItemText, Typography, Button, Stack, Container, ListItemButton, ListItemIcon, Box } from "@mui/material"
+import { List, ListItemText, Typography, Button, Stack, Container, ListItemButton, ListItemIcon, Box, FormControlLabel, Alert } from "@mui/material"
 import { Colors } from "../../styles/theme";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import { SubscribeTf, FooterTitle } from "../../styles/footer";
+import { SubscribeTf, FooterTitle, FooterContainer, FooterInfoAccount, FooterSocial, FooterSubscribe, PrivacyPolicyCheckBox } from "../../styles/footer";
 import SendIcon from "@mui/icons-material/Send";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useTranslation } from "next-i18next";
 
 export default function Footer() {
 
-          const [open, setOpen] = useState(true);
+          const { t } = useTranslation();
+          const [agreed, setAgreed] = useState(false)
+          const [agreedWarning, setAgreedWarning] = useState(false)
 
-          const handleClick = () => {
-                    setOpen(!open);
-          };
+          const handlePrivacyAgreement = (checked: any) => {
+                    setAgreed(checked)
+          }
 
+          const callAlert = () => {
+                    setAgreedWarning(true)
+                    const timeId = setTimeout(() => {
+                              // After 3 seconds set the show value to false
+                              setAgreedWarning(false)
+                    }, 3000)
+
+                    return () => {
+                              clearTimeout(timeId)
+                    }
+          }
+
+          const handleSubscribe = (agreedToTerms: boolean) => {
+                    !agreedToTerms ? callAlert()
+                              :
+                              console.log("do subscribe logic")
+
+          }
           return (
-                    <Box
-                              sx={{
-                                        background: Colors.shaft,
-                                        color: Colors.white,
-                                        p: { xs: 4, md: 10 },
-                                        pt: 12,
-                                        pb: 12,
-                                        fontSize: { xs: '12px', md: '14px' }
-                              }}
-                    >
-                              <Box >
+                    <FooterContainer>
+                              <FooterInfoAccount >
                                         <Box>
                                                   <FooterTitle variant="body1">information</FooterTitle>
-                                                  <List>
+                                                  <List sx={{ textAlign: 'center', color: Colors.secondary }}>
                                                             <ListItemText>
                                                                       <Typography lineHeight={2} variant="caption">
                                                                                 About Us
@@ -58,9 +70,9 @@ export default function Footer() {
                                         </Box>
                                         <Box>
                                                   <FooterTitle variant="body1">my account</FooterTitle>
-                                                  <List>
+                                                  <List sx={{ textAlign: 'center', color: Colors.secondary }}>
                                                             <ListItemText>
-                                                                      <Typography lineHeight={2} variant="caption">
+                                                                      <Typography lineHeight={2} variant="caption" >
                                                                                 Login
                                                                       </Typography>
                                                             </ListItemText>
@@ -81,36 +93,35 @@ export default function Footer() {
                                                             </ListItemText>
                                                   </List>
                                         </Box>
-                                        <Box>
-                                                  <Box
-                                                            sx={{
-                                                                      mt: 4,
-                                                                      color: Colors.dove_gray,
-                                                            }}
-                                                  >
-                                                            <FacebookIcon sx={{ mr: 1 }} />
-                                                            <TwitterIcon sx={{ mr: 1 }} />
-                                                            <InstagramIcon />
-                                                  </Box>
-                                        </Box>
-                                        <Box>
-                                                  <FooterTitle variant="body1">newsletter</FooterTitle>
-                                                  <Stack>
-                                                            <SubscribeTf
-                                                                      color="primary"
-                                                                      label="Email address"
-                                                                      variant="filled"
-                                                            />
-                                                            <Button
-                                                                      startIcon={<SendIcon sx={{ color: Colors.white }} />}
-                                                                      sx={{ mt: 4, mb: 4 }}
-                                                                      variant="contained"
-                                                            >
-                                                                      Subscribe
-                                                            </Button>
-                                                  </Stack>
-                                        </Box>
-                              </Box>
-                    </Box>
+                              </FooterInfoAccount>
+
+                              <FooterSocial>
+                                        <FacebookIcon />
+                                        <TwitterIcon />
+                                        <InstagramIcon />
+                              </FooterSocial>
+
+                              <FooterSubscribe>
+                                        <FooterTitle variant="body1">newsletter</FooterTitle>
+                                        <SubscribeTf
+                                                  color="secondary"
+                                                  label="Email address"
+                                                  variant="filled"
+                                        />
+                                        <Button
+                                                  startIcon={<SendIcon sx={{ color: Colors.white }} />}
+                                                  variant="contained"
+                                                  onClick={() => handleSubscribe(agreed)}
+                                        >
+                                                  Subscribe
+                                        </Button>
+                                        <FormControlLabel control={<PrivacyPolicyCheckBox value={agreed} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handlePrivacyAgreement(e.target.checked)} />} label={t('information.privacy-policy.agree')} />
+                              </FooterSubscribe>
+                              {agreedWarning && (
+                                        <Alert variant="filled" severity="error" sx={{ position: 'fixed', top: '0px', left: '50%', transform: 'translateX(-50%)', width: '250px' }}>
+                                                  {t('information.privacy-policy.agree-warning')}
+                                        </Alert>
+                              )}
+                    </FooterContainer >
           );
 }
--- src/styles/footer/index.ts
+++ src/styles/footer/index.ts
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
-import { TextField, Typography, } from "@mui/material";
+import { Box, TextField, Typography, Checkbox } from "@mui/material";
+import { CheckboxWithLabel } from "formik-mui";
 import { Colors } from "../theme";
 
 
@@ -7,16 +8,59 @@ type TextFieldProps = {
           color: string
 }
 
+export const FooterContainer = styled(Box)(({ theme }: any) => ({
+          alignItems: 'center',
+          backgroundColor: Colors.shaft,
+          color: Colors.white,
+          padding: '2rem',
+          display: 'flex',
+          [theme.breakpoints.down("md")]: {
+                    flexDirection: 'column'
+          },
+          [theme.breakpoints.up("md")]: {
+                    flexDirection: 'row',
+                    justifyContent: 'space-around'
+          },
+})) as typeof Box
+
+export const FooterInfoAccount = styled(Box)(({ theme }: any) => ({
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '5rem',
+          marginBottom: '20px'
+})) as typeof Box
+
+export const FooterSocial = styled(Box)(({ theme }: any) => ({
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '2rem',
+          color: Colors.primary,
+          marginBottom: '20px'
+})) as typeof Box
+
+export const FooterSubscribe = styled(Box)(({ theme }: any) => ({
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '20px',
+          color: Colors.primary,
+          [theme.breakpoints.down("md")]: {
+                    marginBottom: '40px'
+          },
+})) as typeof Box
+
 export const FooterTitle = styled(Typography)(() => ({
+          color: Colors.primary,
           textTransform: "uppercase",
-          marginBottom: "1em",
 })) as typeof Typography
 
+export const PrivacyPolicyCheckBox = styled(Checkbox)(() => ({
+          backgroundColor: 'transparent',
+          color: Colors.secondary,
+}))
 export const SubscribeTf = styled(TextField)<TextFieldProps>(() => ({
           ".MuiInputLabel-root": {
                     color: Colors.secondary,
           },
-
           ".MuiInput-root::before": {
                     borderBottom: `1px solid ${Colors.secondary}`,
           },
