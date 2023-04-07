4820c0521afa4d7f231be5682e389d49f652b77f
banexxl <damjanovic.branislav@gmail.com>
2023-04-07 14:07:09 +0200

    build fix

--- src/store/checkout/checkout-state.interface.ts
+++ src/store/checkout/checkout-state.interface.ts
@@ -1,5 +1,5 @@
 import { ICart } from '@/interfaces/cart/cart.interface';
-import { ICreditCard } from '@/interfaces/checkout/credit-card-form-values.interface';
+import { ICreditCardForm } from '@/interfaces/checkout/credit-card-form-values.interface';
 import { IUserForm } from '@/interfaces/checkout/user-form-values.interface';
 import { AnyAction, Reducer } from '@reduxjs/toolkit';
 import { IPaymentForm } from '../../interfaces/checkout/payment-form-values.interface';
@@ -8,5 +8,5 @@ export interface ICheckoutState {
           checkoutState: any;
           userForm: IUserForm;
           paymentForm: IPaymentForm;
-          creditCardForm: ICreditCard;
+          creditCardForm: ICreditCardForm;
 }
--- src/store/checkout/checkout.selectors.ts
+++ src/store/checkout/checkout.selectors.ts
@@ -2,7 +2,7 @@ import { createSelector } from '@reduxjs/toolkit';
 import { ICheckoutState } from './checkout-state.interface';
 import { IUserForm } from '@/interfaces/checkout/user-form-values.interface';
 import { IPaymentForm } from '@/interfaces/checkout/payment-form-values.interface';
-import { ICreditCard } from '@/interfaces/checkout/credit-card-form-values.interface';
+import { ICreditCardForm } from '@/interfaces/checkout/credit-card-form-values.interface';
 
 const getCheckoutState = (state: ICheckoutState) => state;
 
@@ -10,7 +10,7 @@ const getUserForm = createSelector(getCheckoutState, (checkoutState: ICheckoutSt
 
 const getPaymentForm = createSelector(getCheckoutState, (checkoutState: ICheckoutState): IPaymentForm => checkoutState?.paymentForm)
 
-const creditCardForm = createSelector(getCheckoutState, (checkoutState: ICheckoutState): ICreditCard => checkoutState?.creditCardForm)
+const creditCardForm = createSelector(getCheckoutState, (checkoutState: ICheckoutState): ICreditCardForm => checkoutState?.creditCardForm)
 
 export const checkoutSelectors = {
           getCheckoutState,
