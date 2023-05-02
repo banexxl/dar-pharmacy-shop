4df31d813dc91ef5c541e01808cd0e34948d2557
banexxl <damjanovic.branislav@gmail.com>
2023-05-02 23:13:18 +0200

    small map fix,i dalje imam bag sa markerom

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,42 +1,54 @@
 import React, { useEffect, useRef, useState } from 'react'
 import maplibregl from 'maplibre-gl';
-import { Box } from '@mui/material';
 import { ContactContainer, ContactMap } from '@/styles/contact/contact';
+import { Colors } from '@/styles/theme';
 
-function Contact() {
+export type ContactPageProps = {
+          mapApiKey: string
+}
+
+function Contact(props: ContactPageProps) {
 
           const mapContainer = useRef(null);
           const map = useRef<maplibregl.Map>();
-          const [lat, setLat] = useState(44.01274935)
-          const [long, setLong] = useState(20.9116652)
+          const [lat, setLat] = useState(44.01262879017728)
+          const [long, setLong] = useState(20.912097948648388)
           const [currentLocation, setCurrentLocation] = useState<GeolocationPosition>()
 
+          const options = {
+                    enableHighAccuracy: true,
+                    timeout: 5000,
+                    maximumAge: 0
+          };
+
+          function success(pos: GeolocationPosition) {
+                    setCurrentLocation(pos)
+          }
+
+          function error(err: any) {
+                    console.warn(`ERROR(${err.code}): ${err.message}`);
+          }
+
+          navigator.geolocation.getCurrentPosition(success, error, options);
+
           useEffect(() => {
-
-                    const options = {
-                              enableHighAccuracy: true,
-                              timeout: 5000,
-                              maximumAge: 0
-                    };
-
-                    function success(pos: GeolocationPosition) {
-                              setCurrentLocation(pos)
-                    }
-
-                    function error(err: any) {
-                              console.warn(`ERROR(${err.code}): ${err.message}`);
-                    }
-
-                    navigator.geolocation.getCurrentPosition(success, error, options);
-
-
                     if (map.current) return; //stops map from intializing more than once
                     map.current = new maplibregl.Map({
                               container: mapContainer.current!,
-                              style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${'PFj6lLDUt4uTwv8RNixm'}`,
+                              style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${props.mapApiKey}`,
                               center: [long, lat],
                               zoom: 18,
+                              maplibreLogo: false,
                     });
+
+                    map.current.addControl(new maplibregl.NavigationControl({ showZoom: true, showCompass: true }), 'top-left');
+                    new maplibregl.Marker({ color: Colors.primary })
+                              .setLngLat([20.912097948648388, 44.01262879017728])
+                              .addTo(map.current)
+
+                    return () => {
+                              map.current?.remove;
+                    }
           });
 
           return (
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,26 +1,19 @@
-import Contact from "@/components/contact/contact";
+import Contact, { ContactPageProps } from "@/components/contact/contact";
 import LoadingWheel from "@/components/loading/loading";
 import { UIProvider } from "@/context/ui/ui.context";
-import IProduct from "@/interfaces/product/product.interface";
-import productsServices from "@/services/product.services";
 import theme from "@/styles/theme";
 import { Container, Stack } from "@mui/material";
 import { useTranslation } from "next-i18next"
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 
-const ContactPage = () => {
+const ContactPage = (props: ContactPageProps) => {
 
           const { t } = useTranslation('common')
-
-
           const DynamicThemeProvider = dynamic(() => import("@mui/system/ThemeProvider"), {
                     loading: () => <LoadingWheel isLoading={true} />,
                     ssr: false
           })
-
-          const steps = [t("checkout.shippingAddress"), t("checkout.payment-info"), t("checkout.card-payment"), t("checkout.confirmation")];
 
           return (
                     <DynamicThemeProvider theme={theme}>
@@ -36,7 +29,7 @@ const ContactPage = () => {
                               >
                                         <Stack>
                                                   <UIProvider>
-                                                            <Contact />
+                                                            <Contact mapApiKey={props.mapApiKey} />
                                                   </UIProvider>
                                         </Stack>
                               </Container>
@@ -44,24 +37,11 @@ const ContactPage = () => {
           )
 }
 
-export async function getStaticProps({ locale }: any) {
-
-          const dbData: IProduct[] = await productsServices().getProductForHomePage().then((data: any) => {
-                    return data
-          })
-
-          //notFound: true -> ako vratimo ovo umesto ovog dole, vratice na 404 page tj not found page
-          //redirect: {
-          //           destination: "/nodata"
-          // }  mozemo da proverimo da li podaci uopste postoje, ako ne, mozemo da vratimo ovo, i da uradimo redirect na drugu stranicu
-          //revalidate bi trebao da ponovo odradi getstaticprops logiku
+export async function getStaticProps() {
 
           return {
                     props: {
-                              products: JSON.parse(JSON.stringify(dbData)),
-                              ...(await serverSideTranslations(locale ?? 'sr-RS', [
-                                        'common',
-                              ])),
+                              mapApiKey: process.env.MAP_API_KEY
                     },
           }
 }
