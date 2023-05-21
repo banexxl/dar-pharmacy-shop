b3ed32667937e1d2850ec9bf49c27f2cf16b5937
banexxl <damjanovic.branislav@gmail.com>
2023-05-21 21:54:54 +0200

    cart counter color

--- src/styles/navbar/navbar-cart-counter.ts
+++ src/styles/navbar/navbar-cart-counter.ts
@@ -1,5 +1,6 @@
 import { Typography, styled } from "@mui/material"
+import { Colors } from "../theme"
 
 export const CartCounter = styled(Typography)(({ theme }: any) => ({
-
+          color: Colors.dove_gray
 }))
