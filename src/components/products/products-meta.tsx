df2fff3f99e66be379030fab5273b70649535eae
banexxl <damjanovic.branislav@gmail.com>
2023-07-20 23:08:31 +0200

    da probam dummy build za link

--- src/components/products/products-meta.tsx
+++ src/components/products/products-meta.tsx
@@ -5,7 +5,7 @@ import { ProductMetaWrapper } from "../../styles/product";
 export default function ProductMeta({ product, isScreenToMedium }: any) {
           return (
                     <ProductMetaWrapper>
-                              <Typography variant={isScreenToMedium ? "h6" : "h5"} lineHeight={2} textAlign='center'>
+                              <Typography variant={isScreenToMedium ? "h6" : "h4"} lineHeight={2} textAlign='center'>
                                         {product.name}
                               </Typography>
                               <Typography variant={isScreenToMedium ? "caption" : "body1"} textAlign='center'>
