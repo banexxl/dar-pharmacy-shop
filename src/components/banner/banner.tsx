a7ff0f886068c619212a94ae5e6983e87a5ef56e
banexxl <damjanovic.branislav@gmail.com>
2023-08-04 22:17:31 +0200

    opet drugi link

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -3,17 +3,12 @@ import { useTheme } from "@mui/system"
 import Link from "next/link";
 import { BannerContainer, BannerContent, BannerImage, BannerShopButton, BannerTitle } from "../../styles/banner"
 import { useTranslation } from "next-i18next";
-import { useLoading } from '../../context/loading.context';
 
 export default function Banner() {
 
           const theme = useTheme();
           const isScreenToMedium = useMediaQuery(theme.breakpoints.down("md"))
-          const { setLoading } = useLoading();
           const { t } = useTranslation('common')
-          const handleLinkClick = () => {
-                    setLoading(true);
-          };
 
           return (
                     <BannerContainer>
@@ -23,8 +18,8 @@ export default function Banner() {
                                                   {t('homepage.banner.title')}
                                         </BannerTitle>
                                         {t('homepage.banner.banner-text')}
-                                        <Link href={"/proizvodi/apoteka/alergije/irigacioni-set"}>
-                                                  <BannerShopButton color="primary" onTouchStart={() => handleLinkClick()}>
+                                        <Link href={"/proizvodi/apoteka/bol/bolovi-u-zglobovima-i-misicima"}>
+                                                  <BannerShopButton color="primary">
                                                             {t('homepage.banner.banner-button')}
                                                   </BannerShopButton>
                                         </Link>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,23 +7,19 @@ import store from '../store/store'
 import { appWithTranslation } from 'next-i18next'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
-import { LoadingProvider } from '@/context/loading.context'
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: Session }>) => {
 
           let persistor = persistStore(store)
 
           return (
-                    <LoadingProvider>
-                              <SessionProvider session={session}>
-                                        <Provider store={store}>
-                                                  <PersistGate persistor={persistor}>
-                                                            <Component {...pageProps} />
-                                                  </PersistGate>
-                                        </Provider>
-                              </SessionProvider>
-                    </LoadingProvider>
-
+                    <SessionProvider session={session}>
+                              <Provider store={store}>
+                                        <PersistGate persistor={persistor}>
+                                                  <Component {...pageProps} />
+                                        </PersistGate>
+                              </Provider>
+                    </SessionProvider>
           )
 }
 
