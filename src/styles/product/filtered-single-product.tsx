1db44ae388d564a1377a51964bf43740822d2d44
banexxl <damjanovic.branislav@gmail.com>
2023-08-14 06:44:21 +0200

    drhtanje filtered produkta fix

--- src/styles/product/filtered-single-product.tsx
+++ src/styles/product/filtered-single-product.tsx
@@ -17,7 +17,7 @@ export const FilteredProduct = styled(Box, {
                     position: "relative",
           },
           opacity: isVisible ? '1' : '0',
-          transform: isVisible ? 'translateY(50px)' : 'none',
+          transform: isVisible ? 'translateY(15px)' : 'none',
           visibility: isVisible ? 'visible' : 'hidden',
           transition: 'opacity 1200ms ease-out, transform 600ms ease-out, visibility 1200ms ease-out',
           willChange: 'opacity, transform, visibility',
