bc17d9070c603d608861dc955057ecd482a84c51
branislav.damjanovic@maxsolutions.rs <branislav.damjanovic@maxsolutions.rs>
2023-12-05 08:53:37 +0100

    console log za search sto ne radi

--- src/pages/api/search/product-search-api.ts
+++ src/pages/api/search/product-search-api.ts
@@ -5,20 +5,22 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const ProductSearchApi = async (request: NextApiRequest, response: NextApiResponse) => {
 
-          if (request.method === 'POST') {
-                    try {
-                              const searchedProducts: any = await productsServices().getProductsByNameAndOrManufacturer(request.body);
-                              if (searchedProducts.length > 0) {
-                                        return response.status(200).json({ message: 'Products found!', data: searchedProducts })
-                              } else {
-                                        return response.status(404).json({ error: 'Searched products not found!' });
-                              }
-                    } catch (error) {
-                              return response.status(500).json({ error: 'Internal server error!' });
-                    }
-          } else {
-                    return response.status(405).json({ error: 'Method not allowed!' });
+     if (request.method === 'POST') {
+          try {
+               const searchedProducts: any = await productsServices().getProductsByNameAndOrManufacturer(request.body);
+               if (searchedProducts.length > 0) {
+                    return response.status(200).json({ message: 'Products found!', data: searchedProducts })
+               } else {
+                    return response.status(404).json({ error: 'Searched products not found!' });
+               }
+          } catch (error) {
+               return response.status(500).json({ error: 'Internal server error!' });
           }
+     } else {
+          console.log(response);
+
+          return response.status(405).json({ error: 'Method not allowed!' });
+     }
 };
 
 export default ProductSearchApi
