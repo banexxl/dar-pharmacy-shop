c283a62cae626aafbaaaacbd075b073561e9a962
Branislav Damjanovic <branislav.damjanovic@maxbet.rs>
2024-06-28 13:55:10 +0200

    ubacio stranicu o nama

---
+++ src/pages/informacije/o-nama/index.tsx
@@ -0,0 +1,80 @@
+import { ContactPageProps, ContactMap } from "@/components/contact/contact-map";
+import ContactForm from "@/components/contact/contact-form";
+import LoadingWheel from "@/components/loading/loading";
+import { UIProvider } from "@/context/ui/ui.context";
+import { ContactBox, ContactButton, ContactFormBox, ContactInfoBox, ContactStrongText, ContactText, ContactTitle } from "@/styles/contact/contact";
+import theme from "@/styles/theme";
+import { Box, Container, Stack } from "@mui/material";
+import { useTranslation } from "next-i18next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import SearchBox from "@/components/search/search";
+import AppDrawer from "@/components/navbar/drawer/drawer";
+
+const ContactPage = (props: ContactPageProps) => {
+
+
+     const DynamicThemeProvider = dynamic(() => import("@mui/system/ThemeProvider"), {
+          loading: () => <LoadingWheel />,
+          ssr: false
+     })
+
+     return (
+          <DynamicThemeProvider theme={theme}>
+               <Head>
+                    <title>Kontakt forma</title>
+               </Head>
+               <Container
+                    disableGutters
+                    maxWidth="lg"
+                    sx={{
+                         background: "#fff",
+                    }}
+               >
+                    <Stack>
+                         <UIProvider>
+                              <ContactBox>
+                                   <ContactInfoBox >
+                                        <ContactTitle>
+                                             O nama
+                                        </ContactTitle>
+                                        <Box sx={{ width: '400px', textAlign: 'center' }}>
+                                             <ContactStrongText>
+                                                  Naš moto: Radosno srce-pola Zdravlja!
+                                             </ContactStrongText>
+                                             <ContactStrongText>
+                                                  Kućna dostava Lekova ,Utorkom i Subotom,Kragujevac i Okolina.
+                                             </ContactStrongText>
+                                             <ContactStrongText>
+                                                  Radno Vreme :
+                                                  Radni dani: od 08 do 22h
+                                                  Nedelja: od 08 do 20h
+                                             </ContactStrongText>
+                                             <ContactStrongText>
+                                                  Kralja Aleksnadra Prvog Kardjordjevica 102 lokal9,u sklopu Tc Prostor., Kragujevac 34000
+                                             </ContactStrongText>
+                                        </Box>
+                                   </ContactInfoBox>
+                              </ContactBox>
+                              <SearchBox />
+                              <AppDrawer isScreenToMedium={false} />
+                         </UIProvider>
+                    </Stack>
+               </Container>
+          </DynamicThemeProvider >
+     )
+}
+
+export async function getStaticProps({ locale }: any) {
+
+     return {
+          props: {
+               //...(await serverSideTranslations('sr-RS')),
+               // ...(await serverSideTranslations('sr-RS' ?? context.locale, ['common'], null, ['en-US', 'sr-RS'])),
+               mapApiKey: process.env.GOOGLE_MAPS_API_KEY,
+          },
+     }
+}
+
+export default ContactPage
--- src/styles/contact/contact.tsx
+++ src/styles/contact/contact.tsx
@@ -4,80 +4,83 @@ import { Colors } from "../theme";
 
 
 export const ContactBox = styled(Box)(({ theme }: any) => ({
-          [theme.breakpoints.up("md")]: {
-                    padding: "40px 0px 40px 0px",
-          },
-          marginTop: '130px',
-          display: "flex",
-          flexDirection: 'column',
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "20px 0px 20px 0px",
-          overflow: "hidden",
-          background: Colors.secondary,
-          borderRadius: '10px'
+     [theme.breakpoints.up("md")]: {
+          padding: "40px 0px 40px 0px",
+     },
+     marginTop: '130px',
+     display: "flex",
+     flexDirection: 'column',
+     justifyContent: "center",
+     alignItems: "center",
+     padding: "20px 0px 20px 0px",
+     overflow: "hidden",
+     background: Colors.secondary,
+     borderRadius: '10px'
 }));
 
 export const ContactInfoBox = styled(Box)(({ theme }: any) => ({
-          textAlign: 'center',
-          padding: '15px'
+     textAlign: 'center',
+     padding: '15px'
 
 }));
 
 export const ContactMapBox = styled(Box)(({ theme }: any) => ({
-          borderRadius: '20px',
-          [theme.breakpoints.down("xs")]: {
-                    width: '310px'
-          },
-          [theme.breakpoints.down("sm")]: {
-                    width: '600px'
-          },
-          [theme.breakpoints.down("md")]: {
-                    width: '800px'
-          },
-          [theme.breakpoints.down("xs")]: {
-                    height: '200px'
-          },
-          [theme.breakpoints.down("sm")]: {
-                    height: '400px'
-          },
-          [theme.breakpoints.down("md")]: {
-                    height: '400px'
-          },
+     borderRadius: '20px',
+     [theme.breakpoints.down("xs")]: {
+          width: '310px'
+     },
+     [theme.breakpoints.down("sm")]: {
+          width: '600px'
+     },
+     [theme.breakpoints.down("md")]: {
+          width: '800px'
+     },
+     [theme.breakpoints.down("xs")]: {
+          height: '200px'
+     },
+     [theme.breakpoints.down("sm")]: {
+          height: '400px'
+     },
+     [theme.breakpoints.down("md")]: {
+          height: '400px'
+     },
 }));
 
 export const ContactFormBox = styled(Box)(({ theme }: any) => ({
-          margin: '10px',
-          borderRadius: '20px',
-          padding: '10px',
-          background: Colors.dove_gray,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          gap: '20px',
+     margin: '10px',
+     borderRadius: '20px',
+     padding: '10px',
+     background: Colors.dove_gray,
+     display: 'flex',
+     flexDirection: 'column',
+     alignItems: 'center',
+     justifyContent: 'center',
+     gap: '20px',
 }));
 
 export const ContactTitle = styled(Typography)(({ theme }: any) => ({
-          fontFamily: 'monospace',
-          color: Colors.primary
+     fontFamily: 'monospace',
+     color: Colors.primary,
+     fontWeight: 'bolder',
+     fontSize: '1.5rem',
 }));
 
 export const ContactText = styled(Typography)(({ theme }: any) => ({
-          color: Colors.secondary,
-          fontFamily: 'monospace',
-          textAlign: 'center',
-          alignItems: 'center',
+     color: Colors.secondary,
+     fontFamily: 'monospace',
+     textAlign: 'center',
+     alignItems: 'center',
 
 }));
 
 export const ContactStrongText = styled(Typography)(({ theme }: any) => ({
-          fontFamily: 'monospace',
-          fontStyle: 'italic',
-          fontWeight: 'bolder',
-          color: Colors.primary
+     fontFamily: 'monospace',
+     fontStyle: 'italic',
+     fontWeight: 'bolder',
+     color: Colors.primary,
+     textAlign: 'center',
 }));
 
 export const ContactButton = styled(Button)(({ theme }: any) => ({
-          width: '150px',
+     width: '150px',
 }));
