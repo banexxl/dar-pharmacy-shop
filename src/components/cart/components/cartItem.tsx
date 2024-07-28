752ad2908253134c839503324b513affdec76105
banexxl <damjanovic.branislav@gmail.com>
2024-07-28 22:00:20 +0200

    minor cart mobile fix

--- src/components/cart/components/cartItem.tsx
+++ src/components/cart/components/cartItem.tsx
@@ -4,7 +4,7 @@ import theme, { Colors } from '@/styles/theme'
 import Counter from '@/utils/counter'
 import React from 'react'
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Button } from '@mui/material'
+import { Button, useMediaQuery } from '@mui/material'
 import { useDispatch } from 'react-redux'
 import { removeAllSingleItems } from '@/store/cart/cart.slice'
 
@@ -12,30 +12,31 @@ import { removeAllSingleItems } from '@/store/cart/cart.slice'
 const CartItem = (props: ICartItem) => {
 
      const dispatch = useDispatch()
+     const isScreenToMedium = useMediaQuery(theme.breakpoints.down("md"))
 
      return (
-          <StyledProductRow key={props._id} theme={theme}>
+          <StyledProductRow key={props._id} theme={theme} >
                <StyledProductCell component="th" scope="row" theme={theme}>
                     <CartProductImage src={props.imageURL} />
                </StyledProductCell>
-               <StyledProductName theme={theme} sx={{ transform: 'translateX(-20%)' }}>
+               <StyledProductName theme={theme} sx={{ transform: isScreenToMedium ? 'none' : 'translateX(-20%)' }}>
                     {props.name}
                </StyledProductName>
-               <StyledProductCell align="left" theme={theme} sx={{ transform: 'translateX(-90%)' }}>
+               <StyledProductCell align="left" theme={theme} sx={{ transform: isScreenToMedium ? 'none' : 'translateX(-90%)' }}>
                     {props.quantity}
                </StyledProductCell>
-               <StyledProductCell align="left" theme={theme} sx={{ transform: 'translateX(-70%)' }}>
+               <StyledProductCell align="left" theme={theme} sx={{ transform: isScreenToMedium ? 'none' : 'translateX(-70%)' }}>
                     {props._id.toString().slice(-8).toUpperCase()}
                </StyledProductCell>
-               <StyledProductCell theme={theme} sx={{ transform: 'translateX(-70%)' }}>
+               <StyledProductCell theme={theme} sx={{ transform: isScreenToMedium ? 'none' : 'translateX(-70%)' }}>
                     <Counter _id={props._id} count={props.count}>
                          {props.count}
                     </Counter>
                </StyledProductCell>
-               <StyledProductCell align="left" theme={theme} sx={{ transform: 'translateX(-70%)' }}>
+               <StyledProductCell align="left" theme={theme} sx={{ transform: isScreenToMedium ? 'none' : 'translateX(-70%)' }}>
                     {props.price} rsd
                </StyledProductCell>
-               <StyledProductCell align="left" theme={theme} sx={{ transform: 'translateX(-70%)', display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+               <StyledProductCell align="left" theme={theme} sx={{ transform: isScreenToMedium ? 'none' : 'translateX(-70%)', display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
                     {props.count * props.price} rsd
                     <Button
                          sx={{ backgroundColor: 'transparent' }}
