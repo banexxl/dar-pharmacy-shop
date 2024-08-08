e11a8b55beab2b5d68a45c9e7f4c6d8428f48d00
banexxl <damjanovic.branislav@gmail.com>
2024-08-08 23:49:41 +0200

    da probam captcha v3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,27 +8,30 @@ import { appWithTranslation } from 'next-i18next'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
 import { Analytics } from '@vercel/analytics/react';
+import { ReCaptchaProvider } from "next-recaptcha-v3";
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: Session }>) => {
 
-          let persistor = persistStore(store)
+     let persistor = persistStore(store)
 
-          // useEffect(() => {
-          //           // Initialize i18next
-          //           // You can pass additional configuration options here if needed
-          //           import('i18next').then((i18n) => i18n.default.init());
-          // }, []);
+     // useEffect(() => {
+     //           // Initialize i18next
+     //           // You can pass additional configuration options here if needed
+     //           import('i18next').then((i18n) => i18n.default.init());
+     // }, []);
 
-          return (
-                    <SessionProvider session={session}>
-                              <Provider store={store}>
-                                        <PersistGate persistor={persistor}>
-                                                  <Component {...pageProps} />
-                                        </PersistGate>
-                              </Provider>
-                              <Analytics />
-                    </SessionProvider>
-          )
+     return (
+          <ReCaptchaProvider reCaptchaKey={`${process.env.GOOGLE_CAPTCHA_SITE_KEY}`}>
+               <SessionProvider session={session}>
+                    <Provider store={store}>
+                         <PersistGate persistor={persistor}>
+                              <Component {...pageProps} />
+                         </PersistGate>
+                    </Provider>
+                    <Analytics />
+               </SessionProvider>
+          </ReCaptchaProvider>
+     )
 }
 
 export default appWithTranslation(App)
