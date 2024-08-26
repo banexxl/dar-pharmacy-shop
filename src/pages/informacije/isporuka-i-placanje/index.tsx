3e335e930ff89a74db8c6a2915ee80fae1baa661
banexxl <damjanovic.branislav@gmail.com>
2024-08-26 22:13:55 +0200

    isporuka i iplacanje fix

--- src/pages/informacije/isporuka-i-placanje/index.tsx
+++ src/pages/informacije/isporuka-i-placanje/index.tsx
@@ -4,7 +4,7 @@ import LoadingWheel from "@/components/loading/loading";
 import { UIProvider } from "@/context/ui/ui.context";
 import { ContactBox, ContactFormBox, ContactInfoBox, ContactStrongText, ContactSubTitle, ContactText, ContactTitle } from "@/styles/contact/contact";
 import theme from "@/styles/theme";
-import { Box, Container, Stack } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import { useTranslation } from "next-i18next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import dynamic from "next/dynamic";
@@ -34,80 +34,100 @@ const DeliveryAndPaymentPage = () => {
                >
                     <Stack>
                          <UIProvider>
-                              <ContactBox theme={theme}>
-                                   <ContactInfoBox theme={theme}>
-                                        <ContactTitle>
-                                             Isporuka i plaćanje
-                                        </ContactTitle>
 
-                                        <ContactSubTitle>
-                                             Isporuka
-                                        </ContactSubTitle>
-                                        <ContactText theme={theme}>
-                                             Planirano vreme isporuke od trenutka kreiranja porudžbine je{" "}
-                                             <ContactStrongText theme={theme}>
-                                                  1-5 radnih dana,{" "}
-                                             </ContactStrongText>
-                                             ne računajući vikend, na teritoriji Republike Srbije, putem kurirske službe Post Express.
-                                             Isporuka se ne vrši tokom državnih praznika koji su definisani kao neradni dani, kao ni na adrese u inostranstvu.
-                                        </ContactText>
-                                        <ContactText theme={theme}>
-                                             Dostavu/isporuku za porudžbine do{" "}
-                                             <ContactStrongText theme={theme}>
-                                                  8000 dinara plaća kupac. {" "}
-                                             </ContactStrongText>
-                                             Za sve porudžbine preko{" "}
-                                             <ContactStrongText theme={theme}>
-                                                  8000 dinara isporuka je besplatna.
-                                             </ContactStrongText>
-                                        </ContactText>
+                              <Box sx={{ mt: '70px' }}>
+                                   <Typography marginTop='130px' textAlign='center' fontSize='2rem' fontWeight='bold' >
+                                        Isporuka i plaćanje
+                                   </Typography>
+
+                                   <Typography textAlign='left' fontSize='2rem' fontWeight='bold' ml='20px' >
+                                        Isporuka
+                                   </Typography>
+                                   <Typography textAlign='justify' padding='20px 20px'>
+                                        Planirano vreme isporuke od trenutka kreiranja porudžbine je{" "}
+                                        <Typography component="span" sx={{ textAlignLast: 'left', fontWeight: 'bold', whiteSpace: 'nowrap' }}>
+                                             1-5 radnih dana,{" "}
+                                        </Typography>
+                                        ne računajući vikend, na teritoriji Republike Srbije, putem kurirske službe Post Express.
+                                        Isporuka se ne vrši tokom državnih praznika koji su definisani kao neradni dani, kao ni na adrese u inostranstvu.
+                                   </Typography>
+
+                                   <Typography textAlign='justify' padding='20px 20px' sx={{ textAlignLast: 'left', whiteSpace: 'nowrap' }}>
+                                        Dostavu/isporuku za porudžbine do{" "}
+                                        <Typography
+                                             component="span"
+                                             textAlign='justify'
+
+                                             sx={{ textAlignLast: 'left', fontWeight: 'bold', whiteSpace: 'nowrap' }}
+                                        >
+                                             8000 dinara plaća kupac. {" "}
+                                        </Typography>
+                                        Za sve porudžbine preko{" "}
+                                        <Typography
+                                             component="span"
+                                             textAlign='justify'
+
+                                             sx={{ textAlignLast: 'left', fontWeight: 'bold', whiteSpace: 'nowrap' }}
+                                        >
+                                             8000 dinara isporuka je besplatna.
+                                        </Typography>
+                                   </Typography>
 
 
-                                        <ContactText theme={theme}>
-                                             Za sve ostale detalje cenovnika možete se informisati na stranici Pošte Srbije: {" "}
-                                             <Link href={'http://www.postexpress.rs/struktura/lat/cenovnik/cenovnik-unutrasnji-saobracaj.asp'} target="_blank">
-                                                  <ContactStrongText theme={theme}>
-                                                       cenovnik
-                                                  </ContactStrongText>.
-                                             </Link>
-                                        </ContactText>
-                                        <ContactSubTitle>
-                                             Plaćanje
-                                        </ContactSubTitle>
 
-                                        <ContactText theme={theme}>
-                                             U mogućnosti ste da odaberete:
-                                        </ContactText>
-                                        <ContactStrongText theme={theme}>
-                                             1. Plaćanje pouzećem gotovinski, u trenutku isporuke
-                                        </ContactStrongText>
-                                        <ContactText theme={theme}>
-                                             Ukoliko ste se odlučili za plaćanje pouzećem prilikom poslednjeg koraka pre potvrde kupovine,
-                                             u obavezi ste da paket koji Vam bude dostavljen na adresu platite kuriru na licu mesta.
-                                             Imajte u vidu da kurir možda neće imati sitnog novca da Vam vrati kusur,
-                                             te se potrudite da imate tačnu sumu za plaćanje koja je navedena u potvrdi porudžbine koji ste dobili.
-                                        </ContactText>
-                                        <ContactStrongText theme={theme}>
-                                             2. Plaćanje platnim karticama tokom procesa kupovine na sajtu
-                                        </ContactStrongText>
-                                        <ContactText theme={theme}>
-                                             Ukoliko ste se odlučili za plaćanje Vašom platnom karticom (Dina, Visa, Master i Maestro) online,
-                                             prilikom poslednjeg koraka pre potvrde kupovine, sistem će Vas preusmeriti na sigurnosni link
-                                             OTP banke Srbija AD gde unosite podatke o svojoj platnoj kartici. Ukupna suma i broj pošiljke su
-                                             automatski uneti i ne mogu se menjati.
-                                        </ContactText>
-                                        <ContactText theme={theme}>
-                                             Prilikom unošenja podataka o platnoj kartici, poverljive informacija se prenose u zaštićenoj (kriptovanoj) formi.
-                                             Sigurnost podataka prilikom kupovine, garantuje prihvatilac platnih kartica, OTP banke Srbija AD, pa se tako
-                                             kompletni proces naplate obavlja na stranicama banke. Niti jednog trenutka podaci o platnoj kartici nisu dostupni našem sistemu.
-                                        </ContactText>
-                                        <ContactText theme={theme}>
-                                             Apotekarskoj ustanovi DAR je zabranjeno prodavanje, naručivanje, obezbeđivanje ili razmena podataka potrošača ili informacija o
-                                             platnoj kartici u bilo kojoj formi trećoj strani, sem ako to nije Banka ili nadležni državni organi. Lični i adresni podaci
-                                             potrošača koji se predočavaju Apotekarskoj ustanovi DAR prilikom procesa kupovine smatraju se poslovnom tajnom.
-                                        </ContactText>
-                                   </ContactInfoBox>
-                              </ContactBox>
+                                   <Typography textAlign='justify' padding='20px 20px' sx={{ textAlignLast: 'left', whiteSpace: 'nowrap' }}>
+                                        Za sve ostale detalje cenovnika možete se informisati na stranici Pošte Srbije: {" "}
+                                        <Link
+                                             href={'http://www.postexpress.rs/struktura/lat/cenovnik/cenovnik-unutrasnji-saobracaj.asp'}
+                                             target="_blank"
+                                        >
+                                             <Typography
+                                                  component="span"
+                                                  textAlign='justify'
+                                                  sx={{ textAlignLast: 'left', fontWeight: 'bold', whiteSpace: 'nowrap' }}
+                                             >
+                                                  cenovnik
+                                             </Typography>
+                                        </Link>.
+                                   </Typography>
+
+                                   <Typography marginTop='30px' textAlign='left' fontSize='2rem' fontWeight='bold' ml='20px' >
+                                        Plaćanje
+                                   </Typography>
+
+                                   <Typography textAlign='justify' padding='20px 20px' sx={{ textAlignLast: 'left' }}>
+                                        U mogućnosti ste da odaberete:
+                                   </Typography>
+                                   <Typography textAlign='justify' padding='0px 20px' sx={{ textAlignLast: 'left', fontSize: '1.2rem', fontWeight: 'bold' }}>
+                                        1. Plaćanje pouzećem gotovinski, u trenutku isporuke
+                                   </Typography>
+                                   <Typography textAlign='justify' padding='20px 20px' sx={{ textAlignLast: 'left' }}>
+                                        Ukoliko ste se odlučili za plaćanje pouzećem prilikom poslednjeg koraka pre potvrde kupovine,
+                                        u obavezi ste da paket koji Vam bude dostavljen na adresu platite kuriru na licu mesta.
+                                        Imajte u vidu da kurir možda neće imati sitnog novca da Vam vrati kusur,
+                                        te se potrudite da imate tačnu sumu za plaćanje koja je navedena u potvrdi porudžbine koji ste dobili.
+                                   </Typography>
+                                   <Typography textAlign='justify' padding='0px 20px' sx={{ textAlignLast: 'left', fontSize: '1.2rem', fontWeight: 'bold' }}>
+                                        2. Plaćanje platnim karticama tokom procesa kupovine na sajtu
+                                   </Typography>
+                                   <Typography textAlign='justify' padding='20px 20px' sx={{ textAlignLast: 'left' }}>
+                                        Ukoliko ste se odlučili za plaćanje Vašom platnom karticom (Dina, Visa, Master i Maestro) online,
+                                        prilikom poslednjeg koraka pre potvrde kupovine, sistem će Vas preusmeriti na sigurnosni link
+                                        OTP banke Srbija AD gde unosite podatke o svojoj platnoj kartici. Ukupna suma i broj pošiljke su
+                                        automatski uneti i ne mogu se menjati.
+                                   </Typography>
+                                   <Typography textAlign='justify' padding='20px 20px' sx={{ textAlignLast: 'left' }}>
+                                        Prilikom unošenja podataka o platnoj kartici, poverljive informacija se prenose u zaštićenoj (kriptovanoj) formi.
+                                        Sigurnost podataka prilikom kupovine, garantuje prihvatilac platnih kartica, OTP banke Srbija AD, pa se tako
+                                        kompletni proces naplate obavlja na stranicama banke. Niti jednog trenutka podaci o platnoj kartici nisu dostupni našem sistemu.
+                                   </Typography>
+                                   <Typography textAlign='justify' padding='20px 20px' sx={{ textAlignLast: 'left' }}>
+                                        Apotekarskoj ustanovi DAR je zabranjeno prodavanje, naručivanje, obezbeđivanje ili razmena podataka potrošača ili informacija o
+                                        platnoj kartici u bilo kojoj formi trećoj strani, sem ako to nije Banka ili nadležni državni organi. Lični i adresni podaci
+                                        potrošača koji se predočavaju Apotekarskoj ustanovi DAR prilikom procesa kupovine smatraju se poslovnom tajnom.
+                                   </Typography>
+
+                              </Box>
                               <SearchBox />
                               <AppDrawer isScreenToMedium={false} />
                          </UIProvider>
