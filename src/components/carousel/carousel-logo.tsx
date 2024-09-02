3c68581cc4c11e652c85c6f7faeee7aef3bf1ec1
banexxl <damjanovic.branislav@gmail.com>
2024-09-02 22:01:49 +0200

    link za karusel proizvodjaca

--- src/components/carousel/carousel-logo.tsx
+++ src/components/carousel/carousel-logo.tsx
@@ -56,7 +56,7 @@ const CarouselLogo = (props: CarouselProps) => {
                          props.manufacturers ?
                               props.manufacturers.map((logo: { url: string, name: string, value: string }) => (
                                    <CarouselLogoImgBox key={Math.random()}>
-                                        <Link href={`/${logo.value}?part=1`}>
+                                        <Link href={`/proizvodi-proizvodjac-kategorija/${logo.value}?part=1`}>
                                              <CarouselManufacturerImage src={logo.url} alt={'LOGO'} height={200} width={150} isOnDiscount={false} />
                                         </Link>
                                    </CarouselLogoImgBox>
