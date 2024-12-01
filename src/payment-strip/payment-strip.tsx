1801b8c371f5bc5bb741b02d35c94d07194f0e78
banexxl <damjanovic.branislav@gmail.com>
2024-12-01 20:56:35 +0100

    halk bank i payment strip minor

--- src/payment-strip/payment-strip.tsx
+++ src/payment-strip/payment-strip.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { AppBar, Toolbar, Grid, Box, useMediaQuery } from '@mui/material';
 import Image from 'next/image';
 import theme from '@/styles/theme';
+import Link from 'next/link';
 
 const paymentTypes = [
-     { name: 'Visa', icon: '/cards/visa.png' },
-     { name: 'Mastercard', icon: '/cards/mastercard.png' },
-     { name: 'American Express', icon: '/cards/american-express.png' },
-     { name: 'Maestro', icon: '/cards/maestro.png' },
-     { name: 'Dina card', icon: '/cards/dina-card.png' },
+     { name: 'Visa', icon: '/cards/visa.png', link: 'https://rs.visa.com/' },
+     { name: 'Mastercard', icon: '/cards/mastercard.png', link: 'https://www.mastercard.com/global/en.html' },
+     { name: 'American Express', icon: '/cards/american-express.png', link: 'https://www.americanexpress.com/' },
+     { name: 'Maestro', icon: '/cards/maestro.png', link: 'https://www.mastercard.com/content/brandcenter/standard-mastercard/en/brand-requirement/maestro.html' },
+     { name: 'Halk Bank', icon: '/cards/halkbank.png', link: 'https://www.halkbank.rs/Pocetna.1.html' },
+     { name: 'Dina card', icon: '/cards/dina-card.png', link: 'https://dinacard.nbs.rs/cirilica/' },
 ];
 const PaymentStrip: React.FC = () => {
 
@@ -27,16 +29,22 @@ const PaymentStrip: React.FC = () => {
                                              justifyContent: 'center',
                                              alignItems: 'center',
                                              alignContent: isMobile ? 'center' : 'flex-end',
-                                             paddingBottom: isMobile ? '10px' : '30px'
+                                             paddingBottom: isMobile ? '10px' : '30px',
                                         }}>
-                                        <Image
-                                             style={{ paddingLeft: isMobile ? '10px' : '80px' }}
-                                             src={payment.icon}
-                                             alt={`${payment.name} icon`}
-                                             title={payment.name}
-                                             width={100}
-                                             height={100}
-                                        />
+                                        <Link href={payment.link} target='_blank'>
+                                             <Image
+                                                  style={{
+                                                       paddingLeft: isMobile ? '10px' : '80px',
+                                                       marginTop: payment.name === 'Halk Bank' ? '10px' : '0px',
+                                                       borderRadius: payment.name === 'Halk Bank' ? '10px' : '0px',
+                                                  }}
+                                                  src={payment.icon}
+                                                  alt={`${payment.name} icon`}
+                                                  title={payment.name}
+                                                  width={100}
+                                                  height={100}
+                                             />
+                                        </Link>
                                    </Box>
                               ))}
                          </Grid>
