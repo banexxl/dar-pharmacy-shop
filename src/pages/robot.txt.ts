78dde0bff3f3b4ec31d37d201559611fa5175cf8
banexxl <damjanovic.branislav@gmail.com>
2025-10-27 17:14:22 +0100

    Remove unnecessary Disallow rules from robots.txt for cleaner access control

--- src/pages/robot.txt.ts
+++ src/pages/robot.txt.ts
@@ -5,12 +5,7 @@ const BASE_URL = process.env.BASE_URL || 'https://www.apoteka-dar.rs';
 export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => {
   const content = `
 User-agent: *
-Disallow: /autentifikacija/
-Disallow: /autentifikacija/greska
-Disallow: /autentifikacija/prijava
-Disallow: /autentifikacija/verifikacija-zahteva
 Disallow: /api/
-
 Allow: /
 
 Sitemap: ${BASE_URL}/sitemap.xml
