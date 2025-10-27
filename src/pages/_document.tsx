81c7122290acf18540a62a23105d7bcdaa549b93
banexxl <damjanovic.branislav@gmail.com>
2025-10-27 18:03:15 +0100

    Remove viewport meta tag from _document for improved responsiveness

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="sr">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="google-site-verification" content="jGROhp_tsSx2SYotId-u_cUU1lUPleFTC5eEReOc_7E" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
