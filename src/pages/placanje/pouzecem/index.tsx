e2e5b49c9e05df141b787a691038b85d76f4de83
banexxl <damjanovic.branislav@gmail.com>
2025-10-30 17:45:58 +0100

    Refactor Google Tag Manager integration for improved performance and clarity

--- src/pages/placanje/pouzecem/index.tsx
+++ src/pages/placanje/pouzecem/index.tsx
@@ -63,23 +63,15 @@ const DeliveryConfirmationPage = () => {
 
      return (
           <ReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY} useEnterprise>
-               <Script id="gtm-datalayer" strategy="beforeInteractive">
-                    {`window.dataLayer = window.dataLayer || [];`}
+               <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}></Script>
+               <Script id="gtag-init" strategy="afterInteractive">
+                    {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}');
+  `}
                </Script>
-
-               <Script
-                    id="gtm"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                         __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];
-        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-        var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-        j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl; f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
-    `,
-                    }}
-               />
                <DynamicThemeProvider theme={theme}>
                     <Seo title="Plaćanje/Poručivanje" description="Plaćanje/Poručivanje" url="https://www.apoteka-dar.rs/" />
                     <Container maxWidth="lg" sx={{ py: { xs: 4, md: 6 } }}>
