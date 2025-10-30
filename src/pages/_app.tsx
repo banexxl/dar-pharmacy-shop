9725dd5e6a19a4131021c9e7ed8738f78751150e
banexxl <damjanovic.branislav@gmail.com>
2025-10-30 17:34:00 +0100

    Refactor Google Tag Manager script initialization for clarity and consistency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,22 +34,20 @@ export default function App({
 
      return (
           <SessionProvider session={session}>
-               {/* Initialize dataLayer ASAP */}
-               <Script id="gtm-datalayer" strategy="beforeInteractive">
+               <Script id="gtm-dl" strategy="beforeInteractive">
                     {`window.dataLayer = window.dataLayer || [];`}
                </Script>
-
-               {/* GTM container */}
+               {/* GTM loader */}
                <Script
                     id="gtm"
                     strategy="afterInteractive"
                     dangerouslySetInnerHTML={{
                          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID!}');
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl; f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
           `,
                     }}
                />
